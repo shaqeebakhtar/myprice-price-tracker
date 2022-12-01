26e91a19298c64c5597e4f7cca6f926df3a7f70f
Shaqeeb Akhtar <shaqeebakhtar01@gmail.com>
2022-12-01 12:18:56 +0530

    fix: nodemon crash error

--- myprice-price-tracker-api/server.js
+++ myprice-price-tracker-api/server.js
@@ -1,8 +1,8 @@
-import express from "express";
+const express = require("express");
 const app = express();
 const PORT = 5000;
 
-import { data } from "./utils/SampleData";
+const { data } = require("./SampleData.js");
 
 app.get("/api", (req, res) => {
   res.json(data);
